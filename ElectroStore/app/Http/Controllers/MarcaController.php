047ee1680bb0fcc65b7971f1a4639978ca6d3040
VacnerC <vacner.copa@uab.edu.bo>
2025-08-04 17:54:30 -0400

    Marcas crud

--- ElectroStore/app/Http/Controllers/MarcaController.php
+++ ElectroStore/app/Http/Controllers/MarcaController.php
@@ -2,49 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Marca;
+use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
-        return response()->json(Marca::all());
+        return Marca::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+        ]);
+
+        return Marca::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(Marca $marca)
     {
-        //
+        return $marca;
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Marca $marca)
     {
-        //
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+        ]);
+
+        $marca->update($request->all());
+
+        return $marca;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(Marca $marca)
     {
-        //
+        $marca->delete();
+
+        return response()->json(null, 204);
     }
 }
--- ElectroStore/app/Models/Marca.php
+++ ElectroStore/app/Models/Marca.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Marca extends Model
 {
-    //
-    protected $table = 'marcas';
-    //protected $fillable = 'nombre';
+    use HasFactory;
+
+    protected $fillable = ['nombre'];
+
+    public function productos()
+    {
+        return $this->hasMany(Producto::class);
+    }
 }
