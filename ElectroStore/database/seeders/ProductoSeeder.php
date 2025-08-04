46c105fd4f98dc3d562a9417facb72de6af0f711
VacnerC <vacner.copa@uab.edu.bo>
2025-08-04 17:28:25 -0400

    Datos Productos

--- ElectroStore/database/seeders/ProductoSeeder.php
+++ ElectroStore/database/seeders/ProductoSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class MarcaSeeder extends Seeder
+class ProductoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,19 +14,25 @@ class MarcaSeeder extends Seeder
     public function run(): void
     {
         //
-         DB::table('marcas')->insert([
-             [
-                'nombre' => 'Samsung',
+         DB::table('productos')->insert([
+            [
+                'nombre' => 'Smartphone Galaxy S23',
+                'precio' => 999.99,
+                'marca_id' => 4, // Samsung
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'nombre' => 'Apple',
+                'nombre' => 'iPhone 15 Pro',
+                'precio' => 1199.99,
+                'marca_id' => 5, // Apple
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'nombre' => 'Sony',
+                'nombre' => 'Auriculares WH-1000XM5',
+                'precio' => 399.99,
+                'marca_id' => 6, // Sony
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
