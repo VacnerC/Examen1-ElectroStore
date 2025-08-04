35562356b2bbb76eb223b40a9f24838827d3701f
VacnerC <vacner.copa@uab.edu.bo>
2025-08-04 17:46:03 -0400

    Login creado

--- ElectroStore/routes/api.php
+++ ElectroStore/routes/api.php
@@ -2,13 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ProductoController;
 
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('marcas', MarcaController::class);
+    Route::apiResource('productos', ProductoController::class);
+});
+
+/*
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('usuario', UsuarioController::class);
 Route::apiResource('marca', MarcaController::class);
-
+*/
